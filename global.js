5724cefad7eac402bdafce295489bb377dcd5f01
Sam Ouyang <saouyang@ucsd.edu>
2025-01-25 17:06:49 -0800

    Update global.js

--- global.js
+++ global.js
@@ -5,10 +5,10 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
-  { url: 'contact/', title: 'Contacts' },
-  { url: 'resume/', title: 'Resume' },
+  { url: '/', title: 'Home' },
+  { url: '/projects/', title: 'Projects' },
+  { url: '/contact/', title: 'Contacts' },
+  { url: '/resume/', title: 'Resume' },
   { url: 'https://github.com/samjouyang', title: 'GitHub' },
 ];
 
@@ -18,7 +18,11 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.appendChild(a);
+  // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
