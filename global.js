3d6a7a4598000873a832591a12764d282eeb9f5e
Sam Ouyang <saouyang@ucsd.edu>
2025-01-25 17:57:20 -0800

    Update global.js

--- global.js
+++ global.js
@@ -5,7 +5,7 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: '/', title: 'Home' },
+  { url: '', title: 'Home' },
   { url: '/projects/', title: 'Projects' },
   { url: '/contact/', title: 'Contacts' },
   { url: '/resume/', title: 'Resume' },
@@ -15,35 +15,34 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+// const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 // for (let p of pages) {
 //   let url = p.url;
 //   let title = p.title;
 //   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 // }
 
-// const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-// url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
+const BASE_PATH = '/portfolio';
 
 for (let p of pages) {
-  let url = p.url;
+  let url = p.url.startsWith('/') && !p.url.startsWith('http') ? BASE_PATH + p.url : p.url;
   let title = p.title;
-
-  // Adjust URL if not on the home page and it's not an absolute URL
-  if (!ARE_WE_HOME && !url.startsWith('http')) {
-    url = `..${url}`;
-  }
-
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 
-let a = document.createElement('a');
-a.href = url;
-a.textContent = title;
-nav.append(a);
 
-if (a.host === location.host && a.pathname === location.pathname) {
-  a.classList.add('current');
-}
+
+// url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+
+
+// let a = document.createElement('a');
+// a.href = url;
+// a.textContent = title;
+// nav.append(a);
+
+// if (a.host === location.host && a.pathname === location.pathname) {
+//   a.classList.add('current');
+// }
