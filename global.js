4b15b93e026007edbb1af677e5b8015dd5d7fa1e
Sam Ouyang <saouyang@ucsd.edu>
2025-01-31 17:29:37 -0800

    Update global.js

--- global.js
+++ global.js
@@ -16,7 +16,7 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
+// const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 // for (let p of pages) {
 //   let url = p.url;
