aaa325d41016c87c62c0200ad67d224b3af2e427
Sam Ouyang <saouyang@ucsd.edu>
2025-01-31 16:45:59 -0800

    ok

--- global.js
+++ global.js
@@ -229,3 +229,45 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      console.log(response)
+
+      const data = await response.json();
+      return data; 
+
+      
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+
+}
+
+
+
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // Check if containerElement is valid
+  containerElement.innerHTML = '';
+
+  for (let x of project){
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+`;
+    containerElement.appendChild(article);
+  }
+  
+
+}
